b844fd67f68b7e9032340bb543e3d976c29d9d6f
Deepika Kala <49941789+deepika1998kala@users.noreply.github.com>
2025-07-05 17:46:33 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -62,11 +62,13 @@ await connectDB();
 
 // const PORT = process.env.PORT || 5000;
 
-if(process.env.NODE_ENV !== "production"){
-    const PORT = process.env.PORT || 5000;
-    server.listen(PORT, () => console.log("Server is running on PORT: " + PORT));
-}
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log("Server is running on PORT: " + PORT));
+// if(process.env.NODE_ENV !== "production"){
+//     const PORT = process.env.PORT || 5000;
+//     server.listen(PORT, () => console.log("Server is running on PORT: " + PORT));
+// }
 
 
 //Export the server for vercel
-export default server;
+// export default server;
